98c9ac4cd1ca2576e9e9c6e5a88572ae277c3aed
Emma N. Skye <emma@skyenet.org>
2016-12-04 15:24:36 +0000

    Fix improper RecvVarProxyFn type in RecvProp.

--- src/SDK/Classes/DataTable.hpp
+++ src/SDK/Classes/DataTable.hpp
@@ -49,7 +49,7 @@ class RecvProp {
 		const void* m_pExtraData;
 		RecvProp* m_pArrayProp;
 		void* m_ArrayLengthProxy;
-		RecvVarProxyFn* m_ProxyFn;
+		RecvVarProxyFn m_ProxyFn;
 		void* m_DataTableProxyFn;
 		RecvTable* m_pDataTable;
 		int m_Offset;
