35ea44746f7a6d9bd30661995dbacb4240da3536
Emma N. Skye <emma@skyenet.org>
2016-12-04 15:24:01 +0000

    Add optional RecvProp output parameter to NetVars::GetOffset.

--- src/SDK/Utilities/NetVars.hpp
+++ src/SDK/Utilities/NetVars.hpp
@@ -2,11 +2,14 @@
 
 class NetVars {
 	private:
-		const uintptr_t FindOffset(RecvTable* recv_table, const char* property_name) {
+		const uintptr_t FindOffset(RecvTable* recv_table, const char* property_name, RecvProp** property_ptr = nullptr) {
 			for (int i = 0; i < recv_table->m_nProps; i++) {
 				RecvProp& recv_prop = recv_table->m_pProps[i];
 
 				if (strcmp(recv_prop.m_pVarName, property_name) == 0) {
+					if (property_ptr)
+						*property_ptr = &recv_table->m_pProps[i];
+
 					return recv_prop.m_Offset;
 				}
 
@@ -15,6 +18,9 @@ class NetVars {
 				}
 
 				if (uintptr_t offset = this->FindOffset(recv_prop.m_pDataTable, property_name)) {
+					if (property_ptr)
+						*property_ptr = &recv_table->m_pProps[i];
+
 					return recv_prop.m_Offset + offset;
 				}
 			}
@@ -22,10 +28,10 @@ class NetVars {
 			return 0;
 		}
 	public:
-		const uintptr_t GetOffset(const char* class_name, const char* property_name) {
+		const uintptr_t GetOffset(const char* class_name, const char* property_name, RecvProp** property_ptr = nullptr) {
 			for (ClientClass* class_ptr = clientdll->GetAllClasses(); class_ptr; class_ptr = class_ptr->m_pNext) {
 				if (strcmp(class_ptr->m_pNetworkName, class_name) == 0) {
-					return this->FindOffset(class_ptr->m_pRecvTable, property_name);
+					return this->FindOffset(class_ptr->m_pRecvTable, property_name, property_ptr);
 				}
 			}
 
