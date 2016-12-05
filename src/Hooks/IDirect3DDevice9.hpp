798984f9878a9d1140d917fe0f6f0a2ad7723efc
aixxe <me@aixxe.net>
2016-12-05 12:10:36 +0000

    Add basic configuration settings to user interface.
    Signed-off-by: aixxe <me@aixxe.net>

--- src/Hooks/IDirect3DDevice9.hpp
+++ src/Hooks/IDirect3DDevice9.hpp
@@ -64,32 +64,80 @@ HRESULT __stdcall hkEndScene(IDirect3DDevice9* thisptr) {
 
 	ImGui_ImplDX9_NewFrame();
 
-	// Loop through the game item list.
-	for (const auto& it: ItemDefinitionIndex) {
-		// Get the configuration for the current item.
-		EconomyItem_t& weapon = config.GetWeaponConfiguration(it.first);
-		
-		// Ensure that our settings will be used.
-		if (!weapon.is_valid)
-			weapon.is_valid = true;
+	// Place all item settings under a collapsing header.
+	if (ImGui::CollapsingHeader("Items")) {
+		// Loop through the game item list.
+		for (const auto& item: ItemDefinitionIndex) {
+			// Get the configuration for the current item.
+			EconomyItem_t& weapon = config.GetWeaponConfiguration(item.first);
 
-		// Create a new node in the tree for this item.
-		if (ImGui::TreeNode(it.second.display_name)) {
-			// Add input forms to edit values for this item.
-			ImGui::InputInt(std::string("Item Index##").append(it.second.entity_name).c_str(), &weapon.item_definition_index); 
-			ImGui::InputInt(std::string("Paint Kit##").append(it.second.entity_name).c_str(), &weapon.fallback_paint_kit);
-			ImGui::InputInt(std::string("Seed##").append(it.second.entity_name).c_str(), &weapon.fallback_seed);
-			ImGui::InputInt(std::string("Quality##").append(it.second.entity_name).c_str(), &weapon.entity_quality);
-			ImGui::InputInt(std::string("StatTrak##").append(it.second.entity_name).c_str(), &weapon.fallback_stattrak);
-			ImGui::InputFloat(std::string("Wear##").append(it.second.entity_name).c_str(), &weapon.fallback_wear);
-			ImGui::InputText(std::string("Name Tag##").append(it.second.entity_name).c_str(), weapon.custom_name, 32);
+			// Ensure that our settings will be used.
+			if (!weapon.is_valid)
+				weapon.is_valid = true;
 
-			// Add a placeholer 'Apply' button that calls 'cl_fullupdate'.
-			if (ImGui::Button("Apply", ImVec2(ImGui::GetContentRegionAvail().x, 20)))
-				engine->ClientCmd_Unrestricted("cl_fullupdate");
-			
-			ImGui::TreePop();
+			// Create a new node in the tree for this item.
+			if (ImGui::TreeNode(item.second.display_name)) {
+				// Add input forms to edit values for this item.
+				ImGui::InputInt(std::string("Item Index##").append(item.second.entity_name).c_str(), &weapon.item_definition_index);
+				ImGui::InputInt(std::string("Paint Kit##").append(item.second.entity_name).c_str(), &weapon.fallback_paint_kit);
+				ImGui::InputInt(std::string("Seed##").append(item.second.entity_name).c_str(), &weapon.fallback_seed);
+				ImGui::InputInt(std::string("Quality##").append(item.second.entity_name).c_str(), &weapon.entity_quality);
+				ImGui::InputInt(std::string("StatTrak##").append(item.second.entity_name).c_str(), &weapon.fallback_stattrak);
+				ImGui::InputFloat(std::string("Wear##").append(item.second.entity_name).c_str(), &weapon.fallback_wear);
+				ImGui::InputText(std::string("Name Tag##").append(item.second.entity_name).c_str(), weapon.custom_name, 32);
+
+				// Add a placeholer 'Apply' button that calls 'cl_fullupdate'.
+				if (ImGui::Button("Apply", ImVec2(ImGui::GetContentRegionAvail().x, 20)))
+					engine->ClientCmd_Unrestricted("cl_fullupdate");
+
+				ImGui::TreePop();
+			}
 		}
+	}
+
+	if (ImGui::CollapsingHeader("Presets")) {
+		static std::vector<std::string> presets = config.GetPresets();
+
+		static char preset_filename[64];
+		ImGui::Text("Preset filename");
+		ImGui::InputText("##current_filename", preset_filename, 64);
+		ImGui::SameLine();
+		
+		if (ImGui::Button("New")) {
+			config.SavePreset(preset_filename);
+			presets = config.GetPresets();
+		}
+
+		if (presets.size() >= 1) {
+			for (const std::string& preset: presets) {
+				ImGui::AlignFirstTextHeightToWidgets();
+				
+				ImGui::BulletText(preset.c_str());
+				ImGui::SameLine();
+
+				if (ImGui::Button(std::string("Save##").append(preset).c_str()))
+					config.SavePreset(preset_filename);
+
+				ImGui::SameLine();
+
+				if (ImGui::Button(std::string("Load##").append(preset).c_str())) {
+					config.LoadPreset(preset);
+					engine->ClientCmd_Unrestricted("cl_fullupdate");
+				}
+
+				ImGui::SameLine();
+				
+				if (ImGui::Button(std::string("Delete##").append(preset).c_str()) && config.RemovePreset(preset.c_str()))
+					presets = config.GetPresets();
+
+				ImGui::Spacing();
+			}
+		} else {
+			ImGui::TextWrapped("No presets found. You can create a preset by entering a name above and pressing the 'New' button.");
+		}
+
+		if (ImGui::Button("Refresh"))
+			presets = config.GetPresets();
 	}
 
 	ImGui::Render();
