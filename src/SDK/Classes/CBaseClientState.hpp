9f86c19f0ec0a436aeb4a44781121e93a3893e10
aixxe <me@aixxe.net>
2017-06-24 23:00:54 +0100

    Update CBaseClientState::m_nDeltaTick offset.
    Signed-off-by: aixxe <me@aixxe.net>

--- src/SDK/Classes/CBaseClientState.hpp
+++ src/SDK/Classes/CBaseClientState.hpp
@@ -3,8 +3,8 @@
 class CBaseClientState {
 	public:
 		void ForceFullUpdate() {
-			// m_nDeltaTick: 0x16C
-			*reinterpret_cast<int*>(uintptr_t(this) + 0x16C) = -1;
+			// m_nDeltaTick: 0x174
+			*reinterpret_cast<int*>(uintptr_t(this) + 0x174) = -1;
 		};
 };
 
